2ba5220c80053671b00dab3f497de172874bbcc1
Dbeela <paomagbite@gmail.com>
2021-10-26 17:27:21 +0800

    The pattern functions as intended and does everything I would like it to do.
    All that reamins is to add comments to dlarify certain things and make 
    it readable for the future!

--- main.cpp
+++ main.cpp
@@ -2,28 +2,38 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 // Observer Interface
+static int observer_id;
 class IObserver {
 public:
   IObserver() {
     observer_id++;
   }
+  //~IObserver();
 
-  virtual void update();
+  virtual void update() = 0;
 
-private:
-  static int observer_id;
+
 };
 
 // Observable interface
 class IObservable {
 public:
+  //IObservable();
+  //~IObservable();
+
   virtual void add(IObserver* iObserver) {
     this->observers_.push_back(iObserver);
   }
-  virtual void remove(IObserver* iObserver);
+
+  virtual void remove(IObserver* iObserver) {
+    observers_.erase(std::remove(observers_.begin(), observers_.end(), iObserver), observers_.end());
+  }
+
   virtual void notify() {
+    std::cout << "Observer report\n";
     for (int i = 0; i < observers_.size(); i++) {
       observers_[i]->update();
     }
@@ -33,22 +43,25 @@ private:
   std::vector<IObserver *> observers_;
 };
 
-class Observable : IObservable {
+class Observable : public IObservable {
 public:
+  //Observable();
+  //~Observable();
 
 };
 
-class Observer : IObserver {
+class Observer : public IObserver {
 public:
   Observer(): IObserver() {
-
+    this->id = observer_id;
   }
+  //~Observer();
   void update() {
-
+    std::cout << "Observer " << id << " reporting in!\n";
   }
 
 private:
-  int id = 0;
+  int id;
 
 };
 
@@ -56,6 +69,21 @@ private:
 
 
 int main () {
+
+  Observable observable;
+  Observer observer1;
+  Observer observer2;
+  Observer observer3;
+
+  observable.add(&observer1);
+  observable.add(&observer2);
+  observable.notify();
+
+  observable.add(&observer3);
+  observable.notify();
+
+  observable.remove(&observer2);
+  observable.notify();
 
 
   return 0;
