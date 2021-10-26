8320f81fb17234f63c9f3d8eeb55cf197a85a08f
Dbeela <paomagbite@gmail.com>
2021-10-26 20:16:50 +0800

    Pushing the old observer pattern
    The new observer pattern with proper comments have been programmed

--- main.cpp
+++ main.cpp
@@ -1,80 +1,98 @@
 // Observer pattern
+// Version 0.5.0
+// Written by Dbeela
+// Needs a big revision hence 1.0 will be written based of this one soon
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cstdlib>
 
 // Observer Interface
-static int observer_id;
+//static int observer_id;
 class IObserver {
 public:
   IObserver() {
-    observer_id++;
   }
-  //~IObserver();
-
   virtual void update() = 0;
-
-
 };
 
 // Observable interface
 class IObservable {
 public:
-  //IObservable();
-  //~IObservable();
+  virtual void add(IObserver* iObserver) = 0;
 
-  virtual void add(IObserver* iObserver) {
+  virtual void remove(IObserver* iObserver) = 0;
+
+  virtual void notify() = 0;
+};
+
+// The observed object, child of the IObserver
+class Observable : public IObservable {
+public:
+  // implements add method
+  void add(IObserver* iObserver) override {
     this->observers_.push_back(iObserver);
   }
 
-  virtual void remove(IObserver* iObserver) {
+  // implemnents remove method
+  void remove(IObserver* iObserver) override {
     observers_.erase(std::remove(observers_.begin(), observers_.end(), iObserver), observers_.end());
   }
 
-  virtual void notify() {
+  // implements notify function
+  void notify() override {
     std::cout << "Observer report\n";
     for (int i = 0; i < observers_.size(); i++) {
       observers_[i]->update();
     }
   }
 
+  // provides a random number which the observer is waiting for.
+  int getRanNum() {
+    return rand() % 100;
+  }
+
 private:
   std::vector<IObserver *> observers_;
 };
 
-class Observable : public IObservable {
-public:
-  //Observable();
-  //~Observable();
-
-};
-
+// The object that observes and observable
 class Observer : public IObserver {
 public:
-  Observer(): IObserver() {
-    this->id = observer_id;
+  Observer(Observable* observable) {
+    this->id = observer_id++;
+    this->observable = observable;
   }
-  //~Observer();
-  void update() {
-    std::cout << "Observer " << id << " reporting in!\n";
+  // implements the update method to get the random number and shout it out.
+  void update() override {
+    num = this->observable->getRanNum();
+    std::cout << "Observer " << id << " reporting in and got number: " << num << "\n";
   }
+
 
 private:
   int id;
+  int num;
+
+  static int observer_id;
+
+  Observable* observable;
 
 };
 
-
-
+// I am not sure this must be initilised here...
+int Observer::observer_id = 0;
 
 int main () {
 
+  // Initilization of all objects
   Observable observable;
-  Observer observer1;
-  Observer observer2;
-  Observer observer3;
+  Observer observer1(&observable);
+  Observer observer2(&observable);
+  Observer observer3(&observable);
 
+  // Test Code, tests add and notify
   observable.add(&observer1);
   observable.add(&observer2);
   observable.notify();
@@ -82,6 +100,7 @@ int main () {
   observable.add(&observer3);
   observable.notify();
 
+  // test code testing remove and notify again
   observable.remove(&observer2);
   observable.notify();
 
--- observer0.5.0.cpp
+++ observer0.5.0.cpp
@@ -1,4 +1,7 @@
 // Observer pattern
+// Version 0.5.0
+// Written by Dbeela
+// Needs a big revision hence 1.0 will be written based of this one soon
 
 #include <iostream>
 #include <vector>
@@ -24,14 +27,17 @@ public:
   //IObservable();
   //~IObservable();
 
+  // adds new observer to the observble
   virtual void add(IObserver* iObserver) {
     this->observers_.push_back(iObserver);
   }
 
+  // removes observer
   virtual void remove(IObserver* iObserver) {
     observers_.erase(std::remove(observers_.begin(), observers_.end(), iObserver), observers_.end());
   }
 
+  // notifies all the observers of the obejct
   virtual void notify() {
     std::cout << "Observer report\n";
     for (int i = 0; i < observers_.size(); i++) {
@@ -43,6 +49,8 @@ private:
   std::vector<IObserver *> observers_;
 };
 
+// Should implement this instead of having impenetation in the interface
+// The observed object
 class Observable : public IObservable {
 public:
   //Observable();
@@ -50,6 +58,7 @@ public:
 
 };
 
+// The object that observes and observable
 class Observer : public IObserver {
 public:
   Observer(): IObserver() {
@@ -70,11 +79,13 @@ private:
 
 int main () {
 
+  // Initilization of all objects
   Observable observable;
   Observer observer1;
   Observer observer2;
   Observer observer3;
 
+  // Test Code, tests add and notify
   observable.add(&observer1);
   observable.add(&observer2);
   observable.notify();
@@ -82,6 +93,7 @@ int main () {
   observable.add(&observer3);
   observable.notify();
 
+  // test code testing remove and notify again
   observable.remove(&observer2);
   observable.notify();
 
