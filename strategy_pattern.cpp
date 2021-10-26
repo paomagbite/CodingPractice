88dcf45b38fbb2b0447e32ae7b1f4c2c5a023a20
Dbeela <paomagbite@gmail.com>
2021-10-26 15:05:20 +0800

    ADDED COMMENTS TO CLARIFY FOR FUTURE ME

--- strategy_pattern.cpp
+++ strategy_pattern.cpp
@@ -11,7 +11,7 @@
 #include <iostream>
 #include <string>
 
-// Interface and default option
+// Interface and default flying behavior
 class IFlyingBehavior {
 public:
   virtual void fly() { std::cout << "is Flying\n"; }
@@ -25,24 +25,23 @@ public:
 };
 
 // Duck class, takiung parameters to build desired duck
-// but this does not work atm...
-
 class Duck {
 public:
 
-  IFlyingBehavior *iFlyingBehavior_;
+  IFlyingBehavior *iFlyingBehavior_; // Will use ptr to point towards desired flying behavior
 
+  // Constructor takes a pointer towards an object, to use the desired flying behavior
   Duck(IFlyingBehavior *iFlyingBehavior = nullptr) : iFlyingBehavior_(iFlyingBehavior) {}
 
+  // Function allows for the chainging of flying behavior
   void set_flying_behavior(IFlyingBehavior *iFlyingBehavior)
     {
-        delete this->iFlyingBehavior_;
+        // delete this->iFlyingBehavior_;
         this->iFlyingBehavior_ = iFlyingBehavior;
     }
 
   void fly() {
     this->iFlyingBehavior_->fly();
-    // std::cout << iFlyingBehavior.fuckYou << "\n";
   }
 
 };
@@ -53,12 +52,13 @@ int main () {
   IFlyingBehavior iFlyingBehavior;
   FlyingLong flyingLong;
 
+  // Object must be passed as a reference for Duck to access the flying behavior
   Duck duck1(&iFlyingBehavior);
-  Duck duck2(&flyingLong);
+  // Duck duck2(&flyingLong);
+  duck1.set_flying_behavior(&flyingLong);
 
-  duck1.fly(); // Works as expected
-  duck2.fly(); // This returns the parent functions fly(), which is undesired, it is working now
-  // flyingLong.fly(); // This returns what I want and means the interface works insofar as presumed...
+  duck1.fly();
+  // duck2.fly();
 
   return 0;
 }
